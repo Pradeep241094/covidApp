8e0e72515cb9816352ca203fd3c133885aba9ac0
Pradeep241094 <pradeepprakash24@gmail.com>
2020-06-27 19:19:27 -0400

    added changes to the homescreen and homestack for navigation

--- Routes/Hometsack.js
+++ Routes/Hometsack.js
@@ -12,114 +12,52 @@ import { AppRegistry } from "react-native";
 import PractionerView from '../Screens/PractionerView';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const FeedStack = createStackNavigator();
-
-function HomeStackScreen() {
-  return (
-    <FeedStack.Navigator>
-      <FeedStack.Screen options={{headerShown: false}} name='Splash' component={Splashscreen} />
-      <FeedStack.Screen options={{headerShown: false}} name='Auth' component={AuthScreen} />
-      <FeedStack.Screen name="Home" component={HomeScreen} />
-      <FeedStack.Screen name="Symptoms" component={Symptoms} />
-      {/* other screens */}
-    </FeedStack.Navigator>
-  );
-}
-
-const HomeStackNav = createStackNavigator();
-
-function SymptomsScreen() {
-  return (
-    <HomeStackNav.Navigator>
-      {/* {/* <HomeStachNav.Screen options={{headerShown: false}} name='Splash' component={Splashscreen} />
-      <HomeStachNav.Screen options={{headerShown: false}} name='Auth' component={AuthScreen} /> *} */}
-      <HomeStackNav.Screen  options={{headerShown: false}} name="Symptoms" component={Symptoms} />
-      <HomeStackNav.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
-      {/* other screens */}
-    </HomeStackNav.Navigator>
-  );
-}
-
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStackScreen} />
-      <Tab.Screen name="Symptoms" component={SymptomsScreen} />
-    </Tab.Navigator>
-  );
-}
-
-const RootStack = createStackNavigator();
-
-const Stack = createStackNavigator()
-
-const Tab = createBottomTabNavigator()
-
-// function TabNavigator () {
-// return (
-//   <NavigationContainer>
-//   <Tab.Navigator
-//     screenOptions={({ route }) => ({
-//       tabBarIcon: ({ focused, color, size }) => {
-//         let iconName;
-//         if (route.name === 'Home') {
-//           iconName = focused
-//             ? 'ios-information-circle'
-//             : 'ios-information-circle-outline';
-//         } else if (route.name === 'Symptoms') {
-//           iconName = focused ? 'ios-list-box' : 'ios-list';
-//         }
-
-//         return <Ionicons name={iconName} size={size} color={color} />;
-//       },
-//     })}
-//    tabBarOptions={{
-//     activeTintColor: 'blue',
-//     inactiveTintColor: 'gray',
-//   }}
-//   >
-//     <Tab.Screen name="Home" component={HomeScreen}  style= {{fontSize: 20, }}/>
-//     <Tab.Screen name="Symptoms" component={Symptoms} />
-//     <Tab.Screen name="PractionerView" component={PractionerView} />
-//   </Tab.Navigator>
-// </NavigationContainer>
-// )
-// } 
-
+const Stack = createStackNavigator();
 class Navigator extends React.Component {
-  state={
-    isSignedUp: false
+  state = {
+    isSignedUp: false,
+    tokenPresent: false,
+    Userrole: undefined,
   }
-  
-  componentDidMount(){
+ componentDidMount() {
     this.getData()
   }
 
-async getData () {
-  await AsyncStorage.clear()
-  const value = await AsyncStorage.getItem('token')
-    if(value !== null) {
-      this.setState({ isSignedUp:true })
+  async getData() {
+    const value = JSON.parse(await AsyncStorage.getItem('token'))
+    console.log('value>>>>>>>>>inside function call>>>>>>>>>>>>>>>>>>', value)
+    if (value.data.token !== null) {
+      this.setState({ isSignedUp: true, tokenPresent: true})
     }
-    else { 
-      this.setState({ isSignedUp:false })
+    else {
+      this.setState({ isSignedUp: false, tokenPresent: false })
     }
-}
+  }
 
   render() {
+    const {  isSignedUp } = this.state;
+
     return (
       <>
-      <NavigationContainer>
-      {this.state.isSignedUp ?
-      <RootStack.Navigator>
-        <RootStack.Screen name="Home" component={HomeTabs} />
-      </RootStack.Navigator>
-      :
-      <RootStack.Navigator>
-      <RootStack.Screen name="Home" component={HomeTabs} />
-      </RootStack.Navigator> }
-    </NavigationContainer>
+            <>
+            <NavigationContainer independent={true}>
+            <Stack.Navigator>
+            {
+          isSignedUp ?
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Symptoms" component={Symptoms} />
+              </>
+              :
+              <>
+              <Stack.Screen name="Splash" component={Splashscreen} />
+              <Stack.Screen name="Auth" component={AuthScreen} />
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Symptoms" component={Symptoms} />
+              </>}
+            </Stack.Navigator>
+            </NavigationContainer>
+            </>
       </>
     );
   }
--- newApp.js
+++ newApp.js
@@ -1,104 +1,169 @@
-import * as React from 'react';
-import { Button, Text, View, AppRegistry } from 'react-native';
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-community/async-storage';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import PractionerView from './Screens/PractionerView';
-import HomeScreen from './Screens/HomeScreen';
-import Symptoms from './Screens/Symptoms';
-import { render } from 'react-dom';
-import AuthScreen from './Screens/AuthScene';
-import Splashscreen from './Screens/LoadingScene';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AuthScreen from '../Screens/AuthScene';
+import Splashscreen from '../Screens/LoadingScene';
+import HomeScreen from '../Screens/HomeScreen';
+import Symptoms from '../Screens/Symptoms';
+import Header from '../Screens/Header';
+import { AppRegistry } from "react-native";
+import PractionerView from '../Screens/PractionerView';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const HomeStack = createStackNavigator();
+const FeedStack = createStackNavigator();
+
 function HomeStackScreen() {
- return (
-   <HomeStack.Navigator>
-    <HomeStack.Screen name="Home" component={HomeScreen} />  
-    <HomeStack.Screen name="Symtoms" component={Symptoms} />           
-   </HomeStack.Navigator>
-  );
-}
-const SymptomsStack = createStackNavigator();
-
-function SymptomsStackScreen() {
   return (
-    <SymptomsStack.Navigator>
-      <SymptomsStack.Screen name="Symptoms" component={Symptoms} />
-      <SymptomsStack.Screen name="Home" component={HomeScreen} />
-    </SymptomsStack.Navigator>
+    <FeedStack.Navigator>
+      <FeedStack.Screen options={{ headerShown: false }} name='Splash' component={Splashscreen} />
+      <FeedStack.Screen options={{ headerShown: false }} name='Auth' component={AuthScreen} />
+      <FeedStack.Screen name="Home" component={HomeScreen} />
+      <FeedStack.Screen name="Symptoms" component={Symptoms} />
+      {/* other screens */}
+    </FeedStack.Navigator>
   );
 }
-const Tab = createBottomTabNavigator();
+
+function PractionerStackScreen() {
+  return (
+    <FeedStack.Navigator>
+      <FeedStack.Screen options={{ headerShown: false }} name='Splash' component={Splashscreen} />
+      <FeedStack.Screen options={{ headerShown: false }} name='Auth' component={AuthScreen} />
+      <FeedStack.Screen name="Practioner" component={PractionerView} />
+    </FeedStack.Navigator>
+  )
+}
+
+const HomeStackNav = createStackNavigator();
+
+function SymptomsScreen() {
+  return (
+    <HomeStackNav.Navigator>
+      {/* {/* <HomeStachNav.Screen options={{headerShown: false}} name='Splash' component={Splashscreen} />
+      <HomeStachNav.Screen options={{headerShown: false}} name='Auth' component={AuthScreen} /> *} */}
+      <HomeStackNav.Screen options={{ headerShown: false }} name="Symptoms" component={Symptoms} />
+      <HomeStackNav.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+      {/* other screens */}
+    </HomeStackNav.Navigator>
+  );
+}
 
 
-class App extends React.Component {
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Screen name="Symptoms" component={SymptomsScreen} />
+    </Tab.Navigator>
+  );
+}
 
-  state={
-    isSignedUp: false
+function PractionerTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Practioner" component={PractionerStackScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const RootStack = createStackNavigator();
+
+const Stack = createStackNavigator()
+
+const Tab = createBottomTabNavigator()
+
+// function TabNavigator () {
+// return (
+//   <NavigationContainer>
+//   <Tab.Navigator
+//     screenOptions={({ route }) => ({
+//       tabBarIcon: ({ focused, color, size }) => {
+//         let iconName;
+//         if (route.name === 'Home') {
+//           iconName = focused
+//             ? 'ios-information-circle'
+//             : 'ios-information-circle-outline';
+//         } else if (route.name === 'Symptoms') {
+//           iconName = focused ? 'ios-list-box' : 'ios-list';
+//         }
+
+//         return <Ionicons name={iconName} size={size} color={color} />;
+//       },
+//     })}
+//    tabBarOptions={{
+//     activeTintColor: 'blue',
+//     inactiveTintColor: 'gray',
+//   }}
+//   >
+//     <Tab.Screen name="Home" component={HomeScreen}  style= {{fontSize: 20, }}/>
+//     <Tab.Screen name="Symptoms" component={Symptoms} />
+//     <Tab.Screen name="PractionerView" component={PractionerView} />
+//   </Tab.Navigator>
+// </NavigationContainer>
+// )
+// } 
+
+class Navigator extends React.Component {
+  state = {
+    isSignedUp: false,
+    tokenPresent: false,
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.getData()
   }
 
-async getData () {
-  // await AsyncStorage.clear()
-  const value = await AsyncStorage.getItem('token')
-    if(value !== null) {
-      this.setState({ isSignedUp:true })
-    }
-    else { 
-      this.setState({ isSignedUp:false })
-    }
-}
+  async getData() {
+    await AsyncStorage.clear()
+    const value = await AsyncStorage.getItem('token')
 
-  render () {
+    console.log('token value>>>>>>>>>>', value);
+    if (value !== null) {
+      this.setState({ isSignedUp: true, tokenPresent: true })
+    }
+    else {
+      this.setState({ isSignedUp: false, tokenPresent: false })
+    }
+  }
 
-    const {isSignedUp} = this.state;
-    console.log("isSignedUp>>>>>>>>>>>>>>", isSignedUp);
+  render() {
+    const { tokenPresent } = this.state;
+
+    console.log('tokenPresent', tokenPresent);
+
     return (
       <>
-       {this.state.isSignedUp ?
-       <>
-      <NavigationContainer>
-        <Tab.Navigator
-         screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-           let iconName;
-           if (route.name === 'Home') {
-              iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-            } else if (route.name === 'Symptoms') {
-              iconName = focused
-              ? 'ios-list-box'
-              : 'ios-list';
-            }
-      
-       return <Ionicons name={iconName} size={size} color={color}     />;
-         },
-      })}
-  tabBarOptions={{
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-      }}>
-          <Tab.Screen name="Home" component={HomeStackScreen} />
-          <Tab.Screen name="Symptoms" component= {SymptomsStackScreen} />
-        </Tab.Navigator>
-      </NavigationContainer> 
-      </>
-      :
-      <>
-       <AuthScreen />
-    {/* <View><Text>Hello World</Text></View> */}
-    </>
-    }
+        {
+          tokenPresent ?
+            <NavigationContainer>
+              {this.state.isSignedUp ?
+                <RootStack.Navigator>
+                  <RootStack.Screen name="Practioner" component={PractionerTabs} />
+                </RootStack.Navigator>
+                :
+                <RootStack.Navigator>
+                  <RootStack.Screen name="Practioner" component={PractionerTabs} />
+                </RootStack.Navigator>}
+            </NavigationContainer> :
+            <NavigationContainer>
+              {this.state.isSignedUp ?
+                <RootStack.Navigator>
+                  <RootStack.Screen name="Home" component={HomeTabs} />
+                </RootStack.Navigator>
+                :
+                <RootStack.Navigator>
+                  <RootStack.Screen name="Home" component={HomeTabs} />
+                </RootStack.Navigator>}
+            </NavigationContainer>
+        }
+
       </>
     );
   }
-  }
+}
 
-export default App;
+AppRegistry.registerComponent("Navigator", () => Navigator);
+
+export default Navigator;
